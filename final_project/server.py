4f9b8fad2211b5fdce2e130787533c4c0b2e9fa8
Ahmedsyedfazeel <59147816+Ahmedsyedfazeel@users.noreply.github.com>
2021-11-15 04:21:35 +0500

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -6,19 +6,65 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+    text_to_translate = request.args.get('text_to_translate')
     # Write your code here
+    model_id = 'en-fr'
+    print ('write your content you want to translate')
+    text_to_translate = input()
+
+
+
+# Prepare the Authenticator
+    authenticator = IAMAuthenticator(api_key)
+    language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+    )
+
+    language_translator.set_service_url(api_url )
+
+# Translate
+    translation = language_translator.translate(
+        text=text_to_translate,
+        model_id=model_id).get_result()
+
+# Print results
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+
     return "Translated text to French"
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+@app.route("/FrenchToenglish")
+def FrenchToenglish():
+    text_to_translate = request.args.get('text_to_translate')
     # Write your code here
+     model_id = 'fr-en'
+    print ('écrire le contenu que vous souhaitez')
+    text_to_translate = input()
+
+
+
+# Prepare the Authenticator
+    authenticator = IAMAuthenticator(api_key)
+    language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+    )
+
+    language_translator.set_service_url(api_url )
+
+# Translate
+    translation = language_translator.translate(
+        text=text_to_translate,
+        model_id=model_id).get_result()
+
+# Print results
+    print(json.dumps(translation, indent=2, ensure_ascii=False))
+
     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
-
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
